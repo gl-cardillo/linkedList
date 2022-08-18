cf0b19a4e80e4ef13571493fe17640f8834d241a
gl-cardillo <glucacardillo94@gmail.com>
2022-08-18 16:08:51 +0200

    fix text in test

--- linkedList.test.js
+++ linkedList.test.js
@@ -4,45 +4,45 @@ let node = new Node("node");
 let linkedList = new LinkedList(node);
 
 describe("methods of the linkedList", () => {
-  it("it should append a new node to the end of the linked list", () => {
+  it("getTailValue() should append a new node to the end of the linked list", () => {
     linkedList.append("first node added to the tail");
     expect(linkedList.getTailValue()).toBe("first node added to the tail");
   });
-  it("it should append another node to the end of the linked list", () => {
+  it("getTailValue() should append another node to the end of the linked list", () => {
     linkedList.append("second node added to the tail");
     expect(linkedList.getTailValue()).toBe("second node added to the tail");
   });
 
-  it("it should append a new node to the head of the linked list", () => {
+  it("getHeadValue() should append a new node to the head of the linked list", () => {
     linkedList.prepend("first node added to the head");
     expect(linkedList.getHeadValue()).toBe("first node added to the head");
   });
-  it("it should append another node to the head of the linked list", () => {
+  it("getHeadValue() should append another node to the head of the linked list", () => {
     linkedList.prepend("second node added to the head");
     expect(linkedList.getHeadValue()).toBe("second node added to the head");
   });
 
-  it("it should return the head value", () => {
+  it("getHeadValue() should return the head value", () => {
     linkedList.prepend("new head");
     expect(linkedList.getHeadValue()).toBe("new head");
   });
 
-  it("it should return the tail value", () => {
+  it("getTailValue() should return the tail value", () => {
     linkedList.append("new tail");
     expect(linkedList.getTailValue()).toBe("new tail");
   });
 
-  it("it should return the size of the list", () => {
+  it("getSize() should return the size of the list", () => {
     expect(linkedList.getSize()).toBe(7);
   });
 
-  it("it should return the node at the index", () => {
+  it("at() should return the node at the index", () => {
     expect(linkedList.at(2)).toBe("first node added to the head");
     expect(linkedList.at(4)).toBe("first node added to the tail");
     expect(linkedList.at(9)).toBe("The index 9 is bigger then the list size 7");
   });
 
-  it("it should remove the tail", () => {
+  it("append() should remove the tail", () => {
     linkedList.append("add tail to stay");
     linkedList.append("add tail to remove");
     expect(linkedList.getTailValue()).toBe("add tail to remove");
@@ -50,24 +50,24 @@ describe("methods of the linkedList", () => {
     expect(linkedList.getTailValue()).toBe("add tail to stay");
   });
 
-  it("it should return true if value is contained in a linked list, false viceversa", () => {
+  it("contains() should return true if value is contained in a linked list, false viceversa", () => {
     expect(linkedList.contains("add tail to stay")).toBe(true);
     expect(linkedList.contains("add tail to remove")).toBe(false);
   });
 
-  it("it should return the index of the node with the value requested, if the value is not found returns -1", () => {
+  it("find() should return the index of the node with the value requested, if the value is not found returns -1", () => {
     expect(linkedList.find("add tail to stay")).toBe(6);
     expect(linkedList.find("node")).toBe(2);
     expect(linkedList.find("node doesn't exists")).toBe(-1);
   });
 
-  it("it should display all the linked list node values", () => {
+  it("to String() should display all the linked list node values", () => {
     expect(linkedList.toString()).toBe(
       "(new head) -> (second node added to the head) -> (first node added to the head) -> (node) -> (first node added to the tail) -> (second node added to the tail) -> (new tail) -> (add tail to stay)"
     );
   });
 
-  it("it should add a new node in the list at the requested index", () => {
+  it("removeAt() should add a new node in the list at the requested index", () => {
     expect(linkedList.removeAt("abc")).toBe(
       "Please insert a correct numeric value"
     );
@@ -93,7 +93,7 @@ describe("methods of the linkedList", () => {
     );
   });
 
-  it("it should remove a node in the list at the requested index", () => {
+  it("removeAt() should remove a node in the list at the requested index", () => {
     expect(linkedList.removeAt("abc")).toBe(
       "Please insert a correct numeric value"
     );
